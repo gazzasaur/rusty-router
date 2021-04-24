b3a8932ffd9124794942cc454eed061cdf56ccc1
gaz <graeme.farquharson@gmail.com>
2021-04-23 21:33:08 -0700

    Printing a list of interfaces.

--- rusty-router/src/main.rs
+++ rusty-router/src/main.rs
@@ -1,5 +1,11 @@
-use rusty_router_model::RouteSource;
+use serde_json;
+use rusty_router_model::RustyRouter;
 
 fn main() {
-    println!("{:?}", RouteSource::STATIC);
+    let nl = rusty_router_netlink::NetlinkRustyRouter::new(Box::new(rusty_router_netlink::socket::DefaultNetlinkSocket::new().unwrap()));
+    if let Ok(interfaces) = nl.list_network_interfaces() {
+        if let Ok(output) = serde_json::to_string_pretty(&interfaces) {
+            println!("{}", output);
+        }
+    }
 }
